0fa9441cbf243dfa65c33c2f7d773fdc9e600514
Joel Velis <joelvelis94@gmail.com>
2019-02-14 15:05:05 -0500

    initial set-up

---
+++ bot_game/bot.js
@@ -0,0 +1,4 @@
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+let closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
